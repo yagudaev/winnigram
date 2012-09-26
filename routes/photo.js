c7db5adac7655f04a2f0b4e806f250b2a9fa644f
Michael Yagudaev <michael@yagudaev.com>
2012-09-26 14:37:36 -0500

    made the filters work

--- routes/photo.js
+++ routes/photo.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+	exec = require('child_process').exec;
 	currentID = 0;
 
 function nextID() {
@@ -6,27 +7,10 @@ function nextID() {
 }
 
 function applyFilterAndMoveTempFileToPersistentFolder(imageFileObj, callback) {
+	var destinationPath = 'public/uploads/' + nextID() + '.jpg',
+		command = 'convert -colorspace Gray ' + imageFileObj.path + ' ' + destinationPath;
 
-	fs.readFile(imageFileObj.path, function(err, data){
-
-		applyCrazyFilterToPhoto(data);
-
-		writeDataToNewPath(data, callback)
-	});
-}
-
-function writeDataToNewPath(data, callback) {
-	var destinationPath = process.cwd() + '/public/uploads/' + nextID() + '.jpg';
-
-	console.log('new destination: ' + destinationPath + '\n');
-
-	fs.writeFile(destinationPath, data, function (err) {
-		callback(err);
-	});
-}
-
-function applyCrazyFilterToPhoto(data) {
-	//--> manipulate the photo at this point <--//
+	exec(command, callback);
 }
 
 /*
@@ -39,6 +23,6 @@ exports.upload = function(req, res){
 	console.log(JSON.stringify(photoFileObj, null, 4)); // pretty-print the file object
 	
 	applyFilterAndMoveTempFileToPersistentFolder(photoFileObj, function(err){
-			res.redirect('/');
+		res.redirect('/');
 	});
 };
